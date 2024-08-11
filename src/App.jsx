077fcb986cbec2c66e783e01b38ab78983616005
Amarjot <phullsinghamarjot@gmail.com>
2024-08-11 19:33:52 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -21,7 +21,9 @@ const App = () => {
         <Projects/>
         <About/>
         <Contact/>
+        
       </div>
+      <SpeedInsights />
     </div>
   )
 }
